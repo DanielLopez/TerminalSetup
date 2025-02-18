d5cda8bfc9a910d8172059e6f35e759a5cb1467e
DanielLopez <thinkboxllc@gmail.com>
2025-02-17 20:42:30 -0500

    Add Smear Cursor Plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -958,6 +958,7 @@ require('lazy').setup({
   require 'kickstart.plugins.comment',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.flash',
+  require 'kickstart.plugins.smear',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
